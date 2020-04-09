e09173a185aaf6fb905cb085ed7580c89ae67337
zengyufayangchuyan@outlook.com <zengyufayangchuyan@outlook.com>
2020-04-09 11:21:21 +0800

    配置babel
    配置babel.config.js用于动态添加js中需要引入的方法和API

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,29 @@
 let config = {
-    "presets": [
-        "@babel/env",
-        {
-            "targets": [
-                
-            ]
-        }
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                targets: {
+                    browsers: [
+                        'ie >= 6',
+                        '>2%',
+                        'not dead'
+                    ]
+                },
+                modules: false,
+                useBuiltIns: "usage",
+                corejs: 3
+            }
+        ]
+    ],
+    plugins: [
+        [
+            "@babel/plugin-transform-runtime",
+            {
+                corejs: 2
+            }
+        ]
     ]
-}
+};
 
-module.exports = config
+module.exports = config;
